d543296b8e7d0d7ebe3a6d22623d6d7103787792
tmmaare <aare.tamm@khk.ee>
2013-08-01 23:04:11 +0300

    Loend olemasolevatest töödest

--- views/tasks_index_view.php
+++ views/tasks_index_view.php
@@ -1,5 +1,8 @@
-<?php
-var_dump($tasks);
-foreach ($tasks as $task){
-	//echo
-}
+
+<table class="table table-bordered">
+	<? foreach ($tasks as $task): ?>
+	<tr>
+		<td><a href="<?= BASE_URL ?>tasks/view/<?= $task['task_id'] ?>"><?=$task['task_summary']?></td>
+	</tr>
+<? endforeach ?>
+</table>
