d137036953961769af914131ea82fb039d50831c
Andres <andressoop@gmail.com>
2013-08-01 21:45:27 +0300

    Kujundus

--- views/master_view.php
+++ views/master_view.php
@@ -1,72 +1,231 @@
 <!DOCTYPE html>
-<html class="no-js lt-ie9 lt-ie8 lt-ie7">
-<html class="no-js lt-ie9 lt-ie8">
-<html class="no-js lt-ie9">
-<html class="no-js">
+<html lang="en">
 <head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-	<title>Pealkiri</title>
-	<meta name="description" content="">
-	<meta name="viewport" content="width=device-width">
-
-	<!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
-
-	<link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.1/css/bootstrap-combined.min.css" rel="stylesheet">
-	<script src="<?= ASSETS_URL ?>js/vendor/modernizr-2.6.2.min.js"></script>
-	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-	<script>window.jQuery || document.write('<script src="<?=ASSETS_URL?>js/vendor/jquery-1.9.1.min.js"><\/script>')</script>
-	<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.2/jquery-ui.min.js"></script>
-	<script src="<?= ASSETS_URL ?>js/plugins.js"></script>
-	<script src="<?= ASSETS_URL ?>js/main.js"></script>
-	<script>BASE_URL = '<?=BASE_URL?>'</script>
-
-	<? if (! EMPTY($this->scripts)) : ?>
-		<? foreach ($this->scripts as $script) : ?>
-			<script src="<?= ASSETS_URL ?>js/<?= $script ?>"></script>
-		<? endforeach ?>
-	<? endif ?>
-
+    <meta charset="utf-8">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>Dashboard | Adminstrap</title>
+    <!--jquery-->
+    <link href="assets/jquery/css/jquery-ui-1.8.16.custom.css" rel="stylesheet">
+    <!--bootstrap-->
+    <link href="assets/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <!--app-->
+    <link href="assets/app/css/style.css" rel="stylesheet">
 </head>
 <body>
+<!--Navigation-->
 <div class="navbar navbar-inverse navbar-fixed-top">
-	<div class="navbar-inner">
-		<div class="container">
-			<button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-				<span class="icon-bar"></span>
-			</button>
-			<a class="brand" href="#">Projekti nimi</a>
-			<div class="nav-collapse collapse">
-				<ul class="nav">
-					<li <?= $request->controller == 'apples'  ? 'class="active"'  : '' ?>><a href="<?= BASE_URL ?>apples">Apples</a></li>
-					<li <?= $request->controller == 'oranges' ? 'class="active"' : '' ?>><a href="<?= BASE_URL
-						?>oranges">Oranges</a></li>
-				<? if($auth->logged_in):?>
-					<li><a href="<?= BASE_URL ?>logout">Admin</a></li>
-				<?else:?>
-					<li <?= $request->controller == 'admin' ? 'class="active"' : '' ?>><a href="<?=BASE_URL?>admin">Admin</a></li>
-				<?endif?>
-				</ul>
-			</div>
-			<!--/.nav-collapse -->
-		</div>
-	</div>
+    <div class="navbar-inner">
+        <div class="container-fluid">
+            <a class="brand" href="index.html">
+                <img src="assets/app/img/logo.png" height="45" alt="logo" /></a>
+            <ul id="primary-nav" class="nav">
+                <li><a href="dashboard.html"><i class="nav-icon-9"></i><span>Dashboard</span></a></li>
+                <li class="dropdown"><a href="#" data-toggle="dropdown"><i class="nav-icon-1"></i><span>
+                        Reports</span></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="report.html"><i class="icon-list-alt"></i>&nbsp;Report 1</a></li>
+                        <li><a href="report.html"><i class="icon-list-alt"></i>&nbsp;Report 2</a></li>
+                        <li><a href="report.html"><i class="icon-list-alt"></i>&nbsp;Report 3</a></li>
+                    </ul>
+                </li>
+                <li><a href="forms.html"><i class="nav-icon-12"></i><span>Forms</span></a></li>
+                <li class="dropdown"><a href="#" data-toggle="dropdown"><i class="nav-icon-11"></i><span>
+                        Gallery</span></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="slideshow.html"><i class="icon-play-circle"></i>&nbsp;Fullscreen Slideshow</a></li>
+                        <li><a href="gallery.html"><i class="icon-picture"></i>&nbsp;Image Gallery</a></li>
+                    </ul>
+                </li>
+                <li><a href="calendar.html"><i class="nav-icon-10"></i><span>Calendar</span></a></li>
+            </ul>
+            <ul id="secondary-nav" class="nav pull-right">
+                <li><a data-toggle="modal" href="#myModal"><i class="icon-user icon-white"></i>&nbsp;John
+                        Doe</a></li>
+                <li class="dropdown"><a href="#" data-toggle="dropdown"><i class="icon-cog icon-white">
+                        </i><span>Settings</span></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="#"><i class="icon-wrench"></i>&nbsp;Site Config</a></li>
+                        <li><a href="#"><i class="icon-picture"></i>&nbsp;Themes</a></li>
+                    </ul>
+                </li>
+                <li><a href="index.html"><i class="icon-off icon-white"></i>&nbsp;Logout</a></li>
+            </ul>
+        </div>
+    </div>
 </div>
-<div style="padding: 60px;">
-	<?php
-	require 'views/'.$request->controller.'_'.$request->action.'_view.php';
-	?>
+<!--Profile Form-->
+<div class="modal hide fade" id="myModal">
+    <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">
+            ×</button>
+        <h3>
+            Profile</h3>
+    </div>
+    <div class="modal-body">
+        <form class="form-horizontal">
+            <legend>Contact Info</legend>
+            <div class="control-group">
+                <label class="control-label" for="txtContactName">
+                    Name</label>
+                <div class="controls">
+                    <div class="input-prepend">
+                        <span class="add-on"><i class="icon-user"></i></span>
+                        <input class="span4" id="txtContactName" type="text">
+                    </div>
+                </div>
+            </div>
+            <div class="control-group">
+                <label class="control-label" for="txtPhone">
+                    Phone</label>
+                <div class="controls">
+                    <div class="input-prepend">
+                        <span class="add-on"><i class="icon-comment"></i></span>
+                        <input class="span4" id="txtPhone" type="text">
+                    </div>
+                </div>
+            </div>
+            <div class="control-group">
+                <label class="control-label" for="txtEmail">
+                    E-mail</label>
+                <div class="controls">
+                    <div class="input-prepend">
+                        <span class="add-on"><i class="icon-envelope"></i></span>
+                        <input class="span4" id="txtEmail" type="text">
+                    </div>
+                </div>
+            </div>
+            <legend>Security Info</legend>
+            <div class="control-group">
+                <label class="control-label" for="txtLoginID">
+                    Login ID</label>
+                <div class="controls">
+                    <div class="input-prepend">
+                        <span class="add-on"><i class="icon-user"></i></span>
+                        <input class="span4" id="txtLoginID" type="text" value="admin" disabled>
+                    </div>
+                </div>
+            </div>
+            <div class="control-group">
+                <label class="control-label" for="txtPassword">
+                    Password
+                </label>
+                <div class="controls">
+                    <div class="input-prepend">
+                        <span class="add-on"><i class="icon-lock"></i></span>
+                        <input class="span4" id="txtPassword" type="password">
+                    </div>
+                </div>
+            </div>
+        </form>
+    </div>
+    <div class="modal-footer">
+        <a href="#" class="btn btn-primary" data-dismiss="modal">Save changes</a> <a href="#"
+                                                                                     class="btn" data-dismiss="modal">Cancel</a>
+    </div>
 </div>
-<!--[if lt IE 7]>
-<p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your
-	browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your
-	experience.</p>
-<![endif]-->
+<!--Container-->
+<div class="container-fluid">
+    <!--subnav-->
+    <div class="subnav">
+        <ul class="nav nav-pills">
+            <li><a href="#" onclick="javascript:Dashboard.addWidget();">+ Add Widget</a></li>
+            <li><a href="dashboardprinterfriendly.html" target="_blank">Printer Friendly</a></li>
+            <li><a href="#" onclick="javascript:ShowHelp()">Help</a></li>
+        </ul>
+    </div>
+    <!--Dashboad-->
+    <div id="columns" class="row-fluid">
+        <ul id="widget1" class="column ui-sortable unstyled">
+            <li id="Widget1" class="widget">
+                <div class="widget-head">
+                    <span>Frequency & Recency</span></div>
+                <div class="widget-content">
+                    <iframe id="iframeWidget1" class="widget-iframe" style="overflow: hidden;" src="widgets/widget1.html">
+                    </iframe>
+                </div>
+            </li>
+            <li id="Widget4" class="widget">
+                <div class="widget-head">
+                    <span>Avg. Visit Duration by Territory</span></div>
+                <div class="widget-content">
+                    <iframe id="iframeWidget4" class="widget-iframe" style="overflow: hidden;" src="widgets/widget4.html">
+                    </iframe>
+                </div>
+            </li>
+        </ul>
+        <ul id="widget2" class="column ui-sortable unstyled">
+            <li id="Widget2" class="widget">
+                <div class="widget-head">
+                    <span>Daily Visits</span></div>
+                <div class="widget-content">
+                    <iframe id="iframeWidget3" class="widget-iframe" style="overflow: hidden;" src="widgets/widget3.html">
+                    </iframe>
+                </div>
+            </li>
+            <li id="Widget5" class="widget">
+                <div class="widget-head">
+                    <span>Visits and Pageviews by Devices</span></div>
+                <div class="widget-content">
+                    <iframe id="iframeWidget5" class="widget-iframe" style="overflow: hidden;" src="widgets/widget5.html">
+                    </iframe>
+                </div>
+            </li>
+        </ul>
+        <ul id="widget3" class="column ui-sortable unstyled">
+            <li id="Widget3" class="widget">
+                <div class="widget-head">
+                    <span>Visits by Traffic Type</span></div>
+                <div class="widget-content">
+                    <iframe id="iframeWidget2" class="widget-iframe" style="overflow: hidden;" src="widgets/widget2.html">
+                    </iframe>
+                </div>
+            </li>
+            <li id="Widget6" class="widget">
+                <div class="widget-head">
+                    <span>Live Random Data</span></div>
+                <div class="widget-content">
+                    <iframe id="iframeWidget6" class="widget-iframe" style="overflow: hidden;" src="widgets/widget6.html">
+                    </iframe>
+                </div>
+            </li>
+        </ul>
+    </div>
+</div>
+<!--jquery-->
+<script src="assets/jquery/js/jquery-1.7.2.min.js"></script>
+<!--bootstrap-->
+<script src="assets/bootstrap/js/bootstrap-transition.js"></script>
+<script src="assets/bootstrap/js/bootstrap-alert.js"></script>
+<script src="assets/bootstrap/js/bootstrap-modal.js"></script>
+<script src="assets/bootstrap/js/bootstrap-dropdown.js"></script>
+<script src="assets/bootstrap/js/bootstrap-scrollspy.js"></script>
+<script src="assets/bootstrap/js/bootstrap-tab.js"></script>
+<script src="assets/bootstrap/js/bootstrap-tooltip.js"></script>
+<script src="assets/bootstrap/js/bootstrap-popover.js"></script>
+<script src="assets/bootstrap/js/bootstrap-button.js"></script>
+<script src="assets/bootstrap/js/bootstrap-collapse.js"></script>
+<script src="assets/bootstrap/js/bootstrap-carousel.js"></script>
+<script src="assets/bootstrap/js/bootstrap-typeahead.js"></script>
+<!--jquery ui-->
+<script src="assets/jquery/js/jquery.easing.min.js"></script>
+<script src="assets/jquery/js/jquery-ui-1.8.16.custom.min.js"></script>
+<!--app-->
+<script src="assets/app/js/script.js"></script>
+<!--jquery init-->
+<script>
+    $('document').ready(function () {
+        Dashboard.init();
+    });
 
-<!-- Add your site or application content here -->
+    //Live Tooltip
+    $('body').tooltip({
+        selector: '[rel=tooltip]'
+    });
 
-
+    function ShowHelp() {
+        window.open('help.html', '', 'height=600,width=800,scrollbars=1,resizable=1');
+    }
+</script>
 </body>
 </html>
