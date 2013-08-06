b3743cceb96ff8a4245d6773ed71e1de5ecbc816
EnricoK <enrico.kaasik@gmail.com>
2013-08-06 19:32:46 +0300

    config.sample.php uuendamine

--- config.sample.php
+++ config.sample.php
@@ -1,9 +1,9 @@
 <?php
 
-define('DATABASE_HOSTNAME', 'localhost'); //defineerib konstandi, vasakul nimi, paremal väärtus
+define('DATABASE_HOSTNAME', 'localhost');
 define('DATABASE_USERNAME', 'root');
 define('DATABASE_PASSWORD', '');
-define('DATABASE_DATABASE', 'halo');
-define('DEFAULT_CONTROLLER', 'welcome');
-define('BASE_URL', '/halo/');
-define('ASSETS_URL', '/halo/assets/');
+define('DATABASE_DATABASE', 'taskly');
+define('DEFAULT_CONTROLLER', 'stream');
+define('BASE_URL', '/taskly/');
+define('ASSETS_URL', '/taskly/assets/');
