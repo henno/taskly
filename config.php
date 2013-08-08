f66c0235e54b78a6d7dbd3b9570ae2ec0680c87b
henno <henno.taht@diara.ee>
2013-08-08 19:29:39 +0300

    Kustutatud config.php
    Andresel oli config.php endiselt Gitis sees ja läks kogemata koos jQuery uuendusega üles.

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-define('DATABASE_HOSTNAME', 'localhost'); //defineerib konstandi, vasakul nimi, paremal väärtus
-define('DATABASE_USERNAME', 'root');
-define('DATABASE_PASSWORD', '');
-define('DATABASE_DATABASE', 'taskly');
-define('DEFAULT_CONTROLLER', 'tasks');
-define('BASE_URL', '/taskly/');
-define('ASSETS_URL', '/taskly/assets/');
