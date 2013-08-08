abec534116d2081352e05879eca244326b63e684
kaia <kaia.konsap@khk.ee>
2013-08-08 19:38:03 +0300

    update database

--- doc/task.sql
+++ doc/task.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Aug 01, 2013 at 06:11 PM
+-- Generation Time: Aug 08, 2013 at 04:33 PM
 -- Server version: 5.5.24-log
 -- PHP Version: 5.4.3
 
@@ -32,19 +32,30 @@ CREATE TABLE IF NOT EXISTS `task` (
   `task_id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `task_summary` text NOT NULL,
   `task_reported_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `due_date` datetime NOT NULL,
-  `finished` datetime NOT NULL,
-  `deleted` smallint(5) unsigned NOT NULL DEFAULT '0',
-  PRIMARY KEY (`task_id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
+  `due_date` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `status_id` tinyint(3) unsigned NOT NULL,
+  `deleted` tinyint(5) unsigned NOT NULL DEFAULT '0',
+  PRIMARY KEY (`task_id`),
+  KEY `status_id` (`status_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=6 ;
 
 --
 -- Dumping data for table `task`
 --
 
-INSERT INTO `task` (`task_id`, `task_summary`, `task_reported_time`, `due_date`, `finished`, `deleted`) VALUES
-(1, 'Raske tööö', '2013-08-01 17:36:13', '2013-08-07 07:00:00', '2013-08-17 00:00:00', 0),
-(2, 'väga raske töö', '2013-08-01 21:00:00', '2013-08-09 00:00:00', '2013-08-15 07:16:10', 0);
+INSERT INTO `task` (`task_id`, `task_summary`, `task_reported_time`, `due_date`, `status_id`, `deleted`) VALUES
+(4, 'Esimene töö', '2013-08-08 16:27:22', '2013-08-21 21:00:00', 1, 0),
+(5, 'teine töö', '2013-08-08 16:27:22', '2013-08-15 21:00:00', 2, 0);
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `task`
+--
+ALTER TABLE `task`
+  ADD CONSTRAINT `task_ibfk_1` FOREIGN KEY (`status_id`) REFERENCES `status` (`status_id`);
 SET FOREIGN_KEY_CHECKS=1;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
