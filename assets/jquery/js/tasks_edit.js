150c81eeeba297eb88d7bb74f537e87f418b0436
Matyu <martin.snapper@gmail.com>
2013-08-23 15:55:07 +0300

    poolik edit

---
+++ assets/jquery/js/tasks_edit.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+
+	$(".edit").mouseenter(function() {
+		alert('bla');
+
+	});
+
+	$(".edit").mouseleave(function() {
+
+	});
+});
--- views/tasks_view_view.php
+++ views/tasks_view_view.php
@@ -1,6 +1,18 @@
-<?var_dump($tasks)?>
+<script type="text/javascript" src="http://code.jquery.com/jquery-1.8.3.min.js"></script>
+<script type='text/javascript' src='<?=ASSETS_URL?>jquery\js\tasks_edit.js'></script>
 
-<?if(isset($tasks['task_summary'])&&strlen($tasks['task_summary'])):?>
+<style></style>
+<?if(isset($task['task_summary'])&&strlen($task['task_summary'])):?>
+	<table>
 
-<button>Muuda ülesannet</button>
+		<tr><td>Kokkuvõte</td>
+			<td class="edit" id="tasksummary" name="task_summary"><?=$task['task_summary'];?></td></tr>
+		<tr><td>Tähtaeg</td><td class="edit" id="task_due" name="task_due"><?=$task['task_due'];?></td></tr>
+		<tr><td>Vastutaja</td><td class="edit" id="user_assignee_id"><?=$assignee_name?></td></tr>
+		<tr><td>Teataja</td><td class="edit" id="user_reporter_id"><?=$reporter_name?></td></tr>
+		<tr><td>Aeg</td><td id="task_reported_time"><?=$task['task_reported_time'];?></td></tr>
+
+	</table>
+
+
 <?endif?>
