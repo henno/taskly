c57fbd9142ba0d440192fc299282c0a9d324b319
Henno Täht <henno.taht@gmail.com>
2013-08-27 20:10:47 +0300

    Andrese poolt ekslikult 22.08.2013 functions.php failis muudatuste kogemata üle sõitmise tagasi võtmine

--- functions.php
+++ functions.php
@@ -1,79 +1,129 @@
 <?
+const LOGIN = 1;
+const LOGOUT = 2;
+const TASK_ADD = 3;
+const TASK_EDIT = 4;
+const TASK_REMOVE = 5;
+const COMMENT_ADD = 6;
+const COMMENT_EDIT = 7;
+const COMMENT_REMOVE = 8;
+const STATUS_EDIT = 9;
 /**
  * Display a fancy error page and quit.
+ *
  * @param $error_file_name string The view file of the specific error (in views/errors folder, without _error_view.php suffix)
  */
-function error_out($error_file_name){
+function error_out($error_file_name)
+{
 	require('views/errors/master_error_view.php');
 	exit();
 }
 
 /**
  * Log event in history table
- * @param $task_id int Task associated with the event
- * @param $event_id int Event type
- * @param null $new_value string
- * @param null $old_value string
+ *
+ * @param $event_type_id int Event type
+ * @param $user_id int User's id
+ * @param null $task_id int Task associated with the event
+ * @param null $field string Changed field's name
+ * @param null $new_value string Changed field's new value
+ * @param null $old_value string Changed field's old value
  */
-function tasks_log($task_id, $event_type_id, $field, $new_value = null, $old_value = null){
-    $data['task_id'] = $task_id;
-    $data['user_id'] = 1; // get_user_id
-    $data['event_type_id'] = $event_type_id;
-    $data['new_value'] = $new_value;
-    $data['old_value'] = $old_value;
+function write_log($event_type_id, $user_id = NULL, $task_id = NULL, $field = NULL, $new_value = NULL, $old_value = NULL)
+{
+	$data['task_id'] = $task_id;
+	$data['user_id'] = $user_id;
+	$data['event_type_id'] = $event_type_id;
+	$data['new_value'] = $new_value;
+	$data['old_value'] = $old_value;
 	$data['field'] = $field;
-    insert('log', $data);
+	insert('log', $data);
 }
 
 /**
  * Returns the current user's ID
+ *
  * @return int
  */
-function get_user_id(){
-    return (int)$_SESSION['user_id'];
+function get_user_id()
+{
+	return (int) $_SESSION['user_id'];
 }
+
 function timespan($ts)
 {
 	date_default_timezone_set('Europe/Tallinn');
-	if(!ctype_digit($ts))
+	if (! ctype_digit($ts)) {
 		$ts = strtotime($ts);
+	}
 
 	$diff = time() - $ts;
-	if($diff == 0)
+	if ($diff == 0) {
 		return 'now';
-	elseif($diff > 0)
-	{
+	} elseif ($diff > 0) {
 		$day_diff = floor($diff / 86400);
-		if($day_diff == 0)
-		{
-			if($diff < 60) return 'just now';
-			if($diff < 120) return '1 minute ago';
-			if($diff < 3600) return floor($diff / 60) . ' minutes ago';
-			if($diff < 7200) return '1 hour ago';
-			if($diff < 86400) return floor($diff / 3600) . ' hours ago';
+		if ($day_diff == 0) {
+			if ($diff < 60) {
+				return 'just now';
+			}
+			if ($diff < 120) {
+				return '1 minute ago';
+			}
+			if ($diff < 3600) {
+				return floor($diff / 60).' minutes ago';
+			}
+			if ($diff < 7200) {
+				return '1 hour ago';
+			}
+			if ($diff < 86400) {
+				return floor($diff / 3600).' hours ago';
+			}
 		}
-		if($day_diff == 1) return 'Yesterday';
-		if($day_diff < 7) return $day_diff . ' days ago';
-		if($day_diff < 31) return ceil($day_diff / 7) . ' weeks ago';
-		if($day_diff < 60) return 'last month';
+		if ($day_diff == 1) {
+			return 'Yesterday';
+		}
+		if ($day_diff < 7) {
+			return $day_diff.' days ago';
+		}
+		if ($day_diff < 31) {
+			return ceil($day_diff / 7).' weeks ago';
+		}
+		if ($day_diff < 60) {
+			return 'last month';
+		}
 		return date('F Y', $ts);
-	}
-	else
-	{
+	} else {
 		$diff = abs($diff);
 		$day_diff = floor($diff / 86400);
-		if($day_diff == 0)
-		{
-			if($diff < 120) return 'in a minute';
-			if($diff < 3600) return 'in ' . floor($diff / 60) . ' minutes';
-			if($diff < 7200) return 'in an hour';
-			if($diff < 86400) return 'in ' . floor($diff / 3600) . ' hours';
+		if ($day_diff == 0) {
+			if ($diff < 120) {
+				return 'in a minute';
+			}
+			if ($diff < 3600) {
+				return 'in '.floor($diff / 60).' minutes';
+			}
+			if ($diff < 7200) {
+				return 'in an hour';
+			}
+			if ($diff < 86400) {
+				return 'in '.floor($diff / 3600).' hours';
+			}
 		}
-		if($day_diff == 1) return 'Tomorrow';
-		if($day_diff < 4) return date('l', $ts);
-		if($day_diff < 7 + (7 - date('w'))) return 'next week';
-		if(ceil($day_diff / 7) < 4) return 'in ' . ceil($day_diff / 7) . ' weeks';
-		if(date('n', $ts) == date('n') + 1) return 'next month';
+		if ($day_diff == 1) {
+			return 'Tomorrow';
+		}
+		if ($day_diff < 4) {
+			return date('l', $ts);
+		}
+		if ($day_diff < 7 + (7 - date('w'))) {
+			return 'next week';
+		}
+		if (ceil($day_diff / 7) < 4) {
+			return 'in '.ceil($day_diff / 7).' weeks';
+		}
+		if (date('n', $ts) == date('n') + 1) {
+			return 'next month';
+		}
 		return date('F Y', $ts);
 	}
 }
